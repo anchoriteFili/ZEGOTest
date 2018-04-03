aac53e1a5399445f71eafd529b410436c418dbef
anchoriteFili <feiliyaailiya1@ilcoud.com>
2018-04-03 14:16:36 +0800

    提价一下

---
+++ AudioLive/AudioLiveTests/AudioLiveTests.m
@@ -0,0 +1,39 @@
+//
+//  AudioLiveTests.m
+//  AudioLiveTests
+//
+//  Created by zetafin on 2018/4/3.
+//  Copyright © 2018年 赵宏亚. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AudioLiveTests : XCTestCase
+
+@end
+
+@implementation AudioLiveTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
